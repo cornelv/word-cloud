1afecc99dc8c08d7eef8a7139660640a16f438bf
Cornel Vaideanu <cornel.vaideanu@gmail.com>
2016-06-19 01:23:35 +0300

    implements admin area and encryption

--- config.py
+++ config.py
@@ -2,8 +2,13 @@ import os
 import tornado.web
 import torndb
 from tornado.options import define, options
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+import MySQLdb
 
-from handlers import HomeHandler, ScrapeHandler
+from handlers import HomeHandler, ScrapeHandler, AdminHandler
+
 
 
 define("port", default=8000, help="run on the given port", type=int)
@@ -19,6 +24,7 @@ class Application(tornado.web.Application):
         handlers = [
             (r"/", HomeHandler),
             (r"/scrape/", ScrapeHandler),
+            (r"/admin/", AdminHandler),
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -34,4 +40,49 @@ class Application(tornado.web.Application):
             host=options.mysql_host, database=options.mysql_database,
             user=options.mysql_user, password=options.mysql_password)
 
+        #
+        # create database if required
+        #
+        try:
+            self.db.get('SELECT COUNT(*) from words;')
+
+        except MySQLdb.ProgrammingError: 
+
+            with open('database.sql', 'r') as schema:
+                a = self.db.execute( schema.read() )
+                self.db.reconect()
+
+
+        self.words_pk_salt = 'L})usq/7$X?Ef Cl-9[F+q/)8vkv,Fm^9=:xdb`*@9C>N[]:}es_w?._*|EC.~Wk'
+
+        #
+        # load the private key
+        #
+        if not os.path.isfile('key.pem'):  
+
+            # key file not generated, generate one now        
+            private_key = rsa.generate_private_key(
+                public_exponent=65537,
+                key_size=2048,
+                backend=default_backend()
+            )
+
+            pem = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.BestAvailableEncryption(b'*7K7zv4=8Yh#THm?6WU+')
+            )
+
+            # save the generated key
+            with open('key.pem', 'w') as key_file:
+                key_file.writelines("%s\n" % l for l in pem.splitlines())
+
+        with open("key.pem", "rb") as key_file:
+            self.private_key = serialization.load_pem_private_key(
+                key_file.read(),
+                password=b'*7K7zv4=8Yh#THm?6WU+',
+                backend=default_backend()
+            )
+
+            self.public_key = self.private_key.public_key()
 
--- handlers.py
+++ handlers.py
@@ -4,15 +4,53 @@ from tornado import httpclient, gen
 from bs4 import BeautifulSoup
 import re
 from collections import Counter
-from random import shuffle
+import base64, hashlib
+
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 class BaseHandler(tornado.web.RequestHandler):
     @property
     def db(self):
         return self.application.db
 
+    def encrypt_word(self, word):
+
+    	return self.application.public_key.encrypt(
+				str(word),
+				padding.OAEP(
+					mgf=padding.MGF1(algorithm=hashes.SHA1()),
+					algorithm=hashes.SHA1(),
+					label=None
+				)
+			)
+
+    def decrypt_word(self, word):
+
+    	return self.application.private_key.decrypt(
+    			word,
+				padding.OAEP(
+					mgf=padding.MGF1(algorithm=hashes.SHA1()),
+					algorithm=hashes.SHA1(),
+					label=None
+				)
+    		)
+
+
+class AdminHandler(BaseHandler):
+
+	def get(self):
+		entries = self.db.query("SELECT word, count FROM words order by count DESC LIMIT 2000;")
+
+		words = []
+
+		for word in entries:
+			words.append( (self.decrypt_word(word['word']) , word['count'])) 
+
+		self.render("admin.html", words=words)
 
 class HomeHandler(BaseHandler):
+
     def get(self):
         self.render("home.html")
 
@@ -21,6 +59,25 @@ class ScrapeHandler(BaseHandler):
 
 	def get_words(self, text):
 		return re.compile('\w+').findall(text)
+
+	def save_words(self, words):
+		for word in words:
+
+			# generate the primary key
+			pk = hashlib.sha512(self.application.words_pk_salt + word[0]).digest()
+			pk = base64.urlsafe_b64encode(pk)
+
+			# update the word in DB
+			result = self.db.get("SELECT count(id) as cnt FROM `words` WHERE id = %s", pk) # this should be async
+
+			if result['cnt'] > 0:
+				# update the count in database
+				self.db.execute("UPDATE words set count = count + %s WHERE id = %s;", int(word[1]), pk)
+
+			else:
+				# save the new record in Db
+				result = self.db.execute("INSERT INTO words (id, word, count) VALUES (%s, %s, %s);", \
+										 pk, self.encrypt_word(word[0]), int(word[1]) )
 
 	@gen.coroutine
 	def get_words_from_url(self, url):
@@ -40,6 +97,7 @@ class ScrapeHandler(BaseHandler):
 			c = Counter(words).most_common(100)
 
 			# save the top 100 words in DB
+			self.save_words(c)
 
 
 		except Exception as e:
