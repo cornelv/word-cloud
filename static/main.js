5725872270e2aef4880fb786b099dfcc74cf1df5
Cornel Vaideanu <cornel.vaideanu@gmail.com>
2016-06-19 08:17:49 +0300

    fixes tag cloud

--- static/main.js
+++ static/main.js
@@ -36,6 +36,8 @@
 					var idx = data.result.length;
 					var $template = $(".js-word-cloud .js-word.template");
 					var base_font_size = parseFloat($template.css('font-size'));
+					var max_font_size = base_font_size * 5;
+					var max = data.result[0][1]
 
 					while(idx--) {
 						// generate a random index smaller then x
@@ -48,7 +50,13 @@
 						var clone = $template.clone();
 						clone.html( data.result[rand_idx][0] );
 						clone.removeClass('template');
-						clone.css('font-size', Math.round( base_font_size + parseFloat(data.result[rand_idx][1])/1.2));
+
+						var font_size = Math.round( parseInt(data.result[rand_idx][1]) * max_font_size / max );
+						if(font_size < base_font_size) {
+							font_size = base_font_size
+						}
+
+						clone.css('font-size', font_size);
 						clone.appendTo('.js-word-cloud');
 
 						// remove the item from array
